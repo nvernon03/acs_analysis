7c280e8e5aa9f92597d16105d2b88dc0d2ca72e6
nvernon03 <nvernon32@gmail.com>
2018-06-10 12:42:32 -0400

    cleaned to only include function

--- pull_acs_function.R
+++ pull_acs_function.R
@@ -5,20 +5,6 @@ library(dplyr)
 library(data.table)
 library(bit64)
 
-setwd(Sys.getenv("ACS_DIR")) # set working directory
-
-# scrape the abbreviations for individual states
-webpage <- read_html("https://www2.census.gov/programs-surveys/acs/data/pums/2016/5-Year/")
-st_abr <- webpage %>% 
-  html_nodes("table") %>% 
-  html_nodes("a") %>% 
-  html_attr("href") %>%
-  str_subset(pattern="csv_h") %>% 
-  str_replace("csv_h", "") %>%
-  str_replace(".zip", "")
-
-st_abr <- st_abr[!str_detect(st_abr,pattern="us")]
-
 # FUNCTION TO PULL DATA FOR THE ACS WEBSITE #
 # inputs #
 ## base = the base URL for the dataset
@@ -29,7 +15,7 @@ st_abr <- st_abr[!str_detect(st_abr,pattern="us")]
 
 # example url for 2016, 5 year average, household alaska
 ## "https://www2.census.gov/programs-surveys/acs/data/pums/2016/5-Year/csv_hak.zip"
-acs_pull <- function(base, level, jurisdiction, year, span) {
+acs_pull <- function(base, level, jurisdiction, year, span, delete) {
   full_url <- paste0(base, year, "/", span, "-Year/", "csv_", level, jurisdiction, ".zip") # url for the zip file
   temp <- tempfile() # name of temp file
   tempd <- tempdir() # name of temp directory
@@ -41,32 +27,22 @@ acs_pull <- function(base, level, jurisdiction, year, span) {
   
   temp_dt_list <- list() # list to hold dts with all csvs
   for (f in tempfiles){ # loop through all files
+    gc() # optimizes memory usage
     temp_dt_list[[f]] <- fread(paste0(tempd, "/", f)) # converts csv to a dataframe
+    
+    if (delete==TRUE){ # delete columns is delete is included
+      to_delete <- append(grep('PWGTP', names(temp_dt_list[[f]]), value=TRUE),
+                          names(temp_dt_list[[f]])[startsWith(names(temp_dt_list[[f]]), "F") & endsWith(names(temp_dt_list[[f]]), "P")])
+      temp_dt_list[[f]] <- temp_dt_list[[f]][, {to_delete} := NULL]
+    }
+    file.remove(paste0(tempd, "\\", f)) # delete all csvs
   }
   
   unlink(temp) # delete the temp zip file
   do.call(file.remove, list(list.files(tempd, pattern=".csv", full.names=TRUE))) # delete all csvs
   do.call(file.remove, list(list.files(tempd, pattern=".pdf", full.names=TRUE))) # delete all pdfs
   
+  gc() # optimizes memory usage
   temp_dt <- rbindlist(temp_dt_list) # create a master dt
   return(temp_dt)
 }
-
-# pull all data for the US
-dt_list <- list()  ## create empty list
-for (s in st_abr[1:3]){
-  dt_list[[s]] <- acs_pull(base="https://www2.census.gov/programs-surveys/acs/data/pums/",
-                           level="p",
-                           jurisdiction=s,
-                           year="2016",
-                           span="5")
-  print(s)
-}
-
-# bind dataframes together
-dt <- rbindlist(dt_list)
-rm(dt_list) # remove dataframe list
-
-# save data - create folder if needed
-dir.create(file.path("data"), showWarnings = FALSE)
-write.csv(dt, "data/p_2016_5yr.csv")
