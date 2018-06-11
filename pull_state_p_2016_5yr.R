4cc785c3bc8f2d1695c6043ea2ec2b35b215cc73
nvernon03 <nvernon32@gmail.com>
2018-06-10 22:37:27 -0400

    pull all states 2016 personal 5-yr average

---
+++ pull_state_p_2016_5yr.R
@@ -0,0 +1,41 @@
+# download and merge 2016 5 year PUMS
+library(rvest)
+library(stringr)
+library(dplyr)
+library(data.table)
+library(bit64)
+
+setwd(Sys.getenv("ACS_DIR")) # set working directory
+source("pull_acs_function.R")
+
+# scrape the abbreviations for individual states
+webpage <- read_html("https://www2.census.gov/programs-surveys/acs/data/pums/2016/5-Year/")
+st_abr <- webpage %>% 
+  html_nodes("table") %>% 
+  html_nodes("a") %>% 
+  html_attr("href") %>%
+  str_subset(pattern="csv_h") %>% 
+  str_replace("csv_h", "") %>%
+  str_replace(".zip", "")
+
+st_abr <- st_abr[!str_detect(st_abr,pattern="us")]
+
+# pull all data for the US
+dt_list <- list()  ## create empty list
+for (s in st_abr){ # loop through all states to create datatable
+  dt_list[[s]] <- acs_pull(base="https://www2.census.gov/programs-surveys/acs/data/pums/",
+                           level="p",
+                           jurisdiction="al",
+                           year="2016",
+                           span="5",
+                           delete=TRUE)
+  print(s)
+}
+
+# bind dataframes together
+dt <- rbindlist(dt_list)
+rm(c(dt_list,s)) # remove dataframe list
+
+# save data - create folder if needed
+dir.create(file.path("data"), showWarnings = FALSE)
+write.csv(dt, "data/state_p_2016_5yr.csv")
