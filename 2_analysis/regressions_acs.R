6859d513f757ecaf74409a051f213a9070b06a17
nvernon03 <nvernon32@gmail.com>
2018-07-01 23:01:56 -0400

    functions and regressions added

--- 2_analysis/regressions_acs.R
+++ 2_analysis/regressions_acs.R
@@ -9,24 +9,55 @@
 #+ echo=TRUE, message=FALSE, warning=FALSE
 rm(list=ls()) # clear environment
 library(dplyr)
-library(psych)
 library(data.table)
-library(lazyeval)
 library(ggplot2)
+library(knitr)
+library(car)
 
 #' ### Inputs
 #+ echo=TRUE, message=FALSE, warning=FALSE
 file_name <- "processed_level-p_jur-us_yr-2016_span-1"
 file_folder <- "data"
+wd <- Sys.getenv("ACS_DIR") # working directory location
+file_name_function <- "2_analysis/regression_function"
+endogenous_var <- "log_hr_wagp"
 
 #' ### Read in the data
 #+ echo=TRUE, message=FALSE, warning=FALSE
+setwd(wd) # set working directory
+opts_knit$set(root.dir = wd)
 dt <- fread(paste0(file_folder, "/", file_name, ".csv")) # select dataset to use
+source(paste0(file_name_function, ".R")) # read in functions
 
-#' ### Read in the data
+
+#' ### Naive regression
 #+ echo=TRUE, message=FALSE, warning=FALSE
-# baseline regression
+naive_reg_rhs <- c("race_east_asian","race_white","race_afr_am")
+
+reg_analysis(dt, endogenous_var, naive_reg_rhs,
+             "race_east_asian", "race_white",
+             "race", 100000)
+
+#' ### Baseline regression
+#+ echo=TRUE, message=FALSE, warning=FALSE
 bl_reg_rhs <- c("race_east_asian","race_white","agep","exp","exp2","nativity","sex",
-                names(dt)[grep("deg_", names(dt))])
-bl_reg <- lm(as.formula(paste0("wagp~", paste(bl_reg_rhs, collapse = "+"))), dt)
-summary(bl_reg)
+                names(dt)[grep("deg_", names(dt))][-1], 
+                names(dt)[grep("state_", names(dt))][-1])
+
+reg_analysis(dt, endogenous_var, bl_reg_rhs,
+             "race_east_asian", "race_white",
+             "race", 100000)
+
+
+#' ### Baseline regression - California
+#+ echo=TRUE, message=FALSE, warning=FALSE
+dt_cali <- filter(dt, st == 06)
+setDT(dt_cali)
+
+bl_cali_reg_rhs <- c("race_east_asian","race_white","agep","exp","exp2","nativity","sex",
+                names(dt_cali)[grep("deg_", names(dt_cali))][-1])
+
+
+reg_analysis(dt_cali, endogenous_var, bl_cali_reg_rhs,
+             "race_east_asian", "race_white",
+             "race", 100000)
